cc0a1b2699879f4af3c80268014988b6388727de
Jagriti <jagritibalyan26@gmail.com>
2022-01-25 19:02:29 +0530

    Refactored

--- src/Driver/Main.java
+++ src/Driver/Main.java
@@ -18,21 +18,21 @@ public class Main {
         UserService us = new UserService();
         RideService rs = new RideService();
 
-        Customer c1 = new Customer("Mayank", 'M', 26);
+        Customer c1 = new Customer("Jagriti", 'F', 22);
         CabDriver d1 = new CabDriver("Driver1", 'M', 30, "Swift DL12xyd", new Location(1, 1));
 
         userRepository.addCustomer(c1);
         userRepository.addCabdriver(d1);
 
-        us.updateCustomerLocation("Mayank", new Location(0, 0));
+        us.updateCustomerLocation("Jagriti", new Location(0, 0));
 
-        ArrayList<CabDriver> availableCabdrivers = rs.findRide("Mayank", new Location(0, 0), new Location(5,5));
+        ArrayList<CabDriver> availableCabdrivers = rs.findRide("Jagriti", new Location(0, 0), new Location(5,5));
         System.out.println(availableCabdrivers);
 
-        Ride chosenRide = rs.chooseRide("Mayank", availableCabdrivers.get(0).getName());
+        Ride chosenRide = rs.chooseRide("Jagriti", availableCabdrivers.get(0).getName());
         System.out.println(chosenRide);
 
-        int bill = rs.calculateBill("Mayank");
+        int bill = rs.calculateBill("Jagriti");
 
         System.out.println(bill);
 
